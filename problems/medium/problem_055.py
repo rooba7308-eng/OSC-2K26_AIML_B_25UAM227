a2419d70a7af3eaa6308b82e54cd124d3a93f6ff
rooba7308-eng <rooba7308@gmail.com>
2026-01-28 08:29:52 +0530

    problem_55 solved

--- problems/medium/problem_055.py
+++ problems/medium/problem_055.py
@@ -5,16 +5,6 @@ Difficulty: Medium
 """
 
 def process_budget_data(data):
-    
-    results = []
-    
-    for i in range(len(data) + 1):
-        if i < len(data):
-             results.append(data[i] * 2)
-        else:
-             
-             results.append(data[i])
-    return results
-
+   return[value *2 for value in data]
 values = [10, 20, 30]
 print(process_budget_data(values))
